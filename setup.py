5152cb71743b7fb47d67794a9c34f7c5c7b2e078
Boots-bots <faustobottazzini@gmail.com>
2025-07-10 16:19:19 -0300

    f

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="settings",
     version="0.1",
     packages=find_packages(),
+    include_package_data=True,
     description="Librería de configuraciones reutilizables",
     author="Fausto Bottazzini",
 )
